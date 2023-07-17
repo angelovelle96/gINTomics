f54ace6484b55a6bd07d5fdb8fa7a1a35819f4ce
angeve96 <angeve89@gmail.com>
2023-07-17 16:24:45 +0200

    Documentation

--- R/multiomics.R
+++ R/multiomics.R
@@ -1,8 +1,35 @@
 
+#' Complete Multi-Omics integration
+#' @description
+#' This function will perform a complete Multi-Omics integration on a
+#' MultiAssayExperiment
 #' @import MultiAssayExperiment SummarizedExperiment
+#'
+#' @param data A MultiAssayExperiment. It can be generated exploiting the
+#' **generate_multiassay** function.
+#' @param interactions_met **interactions** as for **run_met_integration**
+#' @param interactions_miRNA_target miRNA-target interactions as requested by
+#'  **run_tf_integration**
+#' @param interactions_tf TF-target interactions as requested by
+#'  **run_tf_integration**
+#' @param interactions_tf_miRNA TF-target interactions as
+#' requested by **run_tf_integration**
+#' @param RNAseq logical. Are gene expression data obtained from RNA
+#' sequencing ? Default is set to TRUE
+#' @param miRNAseq logical. Are miRNA expression data obtained from miRNA
+#' sequencing ? Default is set to TRUE
+#' @param normalize_miRNA_expr logical.Should miRNA expression data be
+#' normalized ? Default is set to TRUE
+#' @param normalize_gene_expr logical.Should gene expression data be
+#' normalized ? Default is set to TRUE
+#' @param norm_method_gene_expr Normalization method to be used for gene
+#' expression data. One of "TMM" (default), "TMMwsp", "RLE", "upperquartile",
+#' "none".
+#' @param norm_method_miRNA_expr Normalization method to be used for miRNA
+#' expression data. One of "TMM" (default), "TMMwsp", "RLE", "upperquartile",
+#' "none".
 #' @export
 run_multiomics <- function(data,
-                           use_cnv_res=T,
                            interactions_met=NULL,
                            interactions_miRNA_target=NULL,
                            interactions_tf=NULL,
@@ -159,12 +186,6 @@ run_multiomics <- function(data,
       mirna_target_res$data$response_var <- tmp
     }
 
-
-
-
-
-#proseguo con le altre integrazioni
-
   ans <- new("MultiOmics", Filter(Negate(is.null),
                                   list(gene_cnv_res=gene_cnv_res,
                                        mirna_cnv_res=mirna_cnv_res,
@@ -175,7 +196,25 @@ run_multiomics <- function(data,
   return(ans)
 }
 
-####################################################################
+#' Integration of expression and Copy Number Variations
+#' @description
+#' This function will perform an integration of expression data and Copy Number
+#' Variations data
+#' @param expression Matrix or data.frame containing the expression values
+#' for each model. Rows represent samples, while each column represents
+#' the different response variables of the models.
+#' @param cnv_data Matrix or data.frame containing the Copy Number variation
+#' status for the models. Rows represent samples, while columns represent
+#' the different covariates. If **interactions** are not provided, they will be
+#' automatically generated and for each gene contained in **expression**
+#' the model will look for the same gene in **cnv_data**
+#' @param sequencing_data logical. Are expression data obtained from RNA
+#' sequencing ? Default is set to TRUE
+#' @param normalize logical.Should expression data be
+#' normalized ? Default is set to TRUE
+#' @param norm_method Normalization method to be used for
+#' expression data. One of "TMM" (default), "TMMwsp", "RLE", "upperquartile",
+#' "none".
 #' @export
 run_cnv_integration <- function(expression,
                                 cnv_data,
@@ -205,7 +244,25 @@ run_cnv_integration <- function(expression,
 }
 
 
-####################################################################
+#' Integration of expression and methylation
+#' @description
+#' This function will perform an integration of expression data and methylation
+#'  data
+#' @param expression Matrix or data.frame containing the expression values
+#' for each model. Rows represent samples, while each column represents
+#' the different response variables of the models.
+#' @param methylation Matrix or data.frame containing the methylation
+#' values for the models. Rows represent samples, while columns represent
+#' the different covariates. If **interactions** are not provided, they will be
+#' automatically generated and for each gene contained in **expression**
+#' the model will look for the same gene in **methylation**
+#' @param sequencing_data logical. Are expression data obtained from RNA
+#' sequencing ? Default is set to TRUE
+#' @param normalize logical.Should expression data be
+#' normalized ? Default is set to TRUE
+#' @param norm_method Normalization method to be used for
+#' expression data. One of "TMM" (default), "TMMwsp", "RLE", "upperquartile",
+#' "none".
 #' @export
 
 run_met_integration <- function( expression,
@@ -236,7 +293,40 @@ run_met_integration <- function( expression,
 }
 
 
-####################################################################
+#' Integration of expression and Transcription Factors / Generic Regulators
+#' @description
+#' This function will perform an integration of gene/miRNA expression data
+#' and Transcription Factors expression. Moreover, every type of regulator can
+#' be provided to the function as covariate through the **tf_expression**
+#' argument. Interactions for TF-target, miRNA-target and TF-miRNA integration
+#' will be automatically downloaded by the function as defined by the **type**
+#' argument. Other types of interactions should be provided through the
+#' **interactions** argument.
+#' @param expression Matrix or data.frame containing the expression values
+#' for each model. Rows represent samples, while each column represents
+#' the different response variables of the models.
+#' @param tf_expression Matrix or data.frame containing the expression
+#' values for the models. Rows represent samples, while columns represent
+#' the different covariates. If not provided, it will be set equal to
+#' **expression**.
+#' @param interactions A list of character vectors containing the interactions
+#' between response variable and covariates. The names of the list should
+#' match the response variables while the character contained in each element
+#' of the list should match the covariates. If NULL (default), the interactions
+#' will be automatically downloaded according to the **type** argument.
+#' @param type A character defining the type of regulation under analysis.
+#' Should be one of "tf_miRNA", "tf", "miRNA_target".
+#' @param species species information for interactions download. Fully
+#' supported species are "hsa" (default) and "mmu".
+#' @param sequencing_data logical. Are expression data obtained from RNA
+#' sequencing ? Default is set to TRUE
+#' @param normalize logical.Should expression data be
+#' normalized ? Default is set to TRUE
+#' @param norm_method Normalization method to be used for
+#' expression data. One of "TMM" (default), "TMMwsp", "RLE", "upperquartile",
+#' "none".
+#' @param normalize_cov Same as **normalize** but for covariates.
+#' @param norm_method_cov Same as **norm_method** but for covariates.
 #' @export
 
 run_tf_integration <- function( expression,
