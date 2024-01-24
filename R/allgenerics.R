603c89835113e6879282e355a8b2296d77ce6050
angeve96 <angeve89@gmail.com>
2024-01-24 11:04:16 +0100

    remove extract_data

--- R/allgenerics.R
+++ R/allgenerics.R
@@ -2,6 +2,3 @@
 setGeneric("extract_model_res", function(model_results, ...)
   standardGeneric("extract_model_res"))
 
-#DA ELIMINARE
-setGeneric("extract_data", function(model_results, ...)
-  standardGeneric("extract_data"))
